b80d4ff1e3374cbbac90bd8b3bd3f452563e1cdb
khronos <khronos@khronos-kali.khronos.tech>
2023-11-29 21:10:19 -0800

    Created fetch_ssl_blacklist function

--- ThreatScope.py
+++ ThreatScope.py
@@ -1,10 +1,40 @@
 #!/usr/bin/env python3
 
+import requests
+from io import StringIO
+import csv
 
+def fetch_ssl_blacklist():
+    url = "https://sslbl.abuse.ch/blacklist/sslipblacklist_aggressive.csv"
 
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        data = StringIO(response.text)
+        headers = None
+        
+        while True:
+            line = data.readline()
+
+            if 'Firstseen,DstIP,DstPort' in line:
+                headers = line.strip('# \r\n').split(',')
+                break
+
+        csv_reader = csv.DictReader(data, fieldnames=headers)
+
+        blacklist = [row for row in csv_reader if row]
+        return blacklist
+
+    except requests.RequestException as e:
+        print(f"Error fetching data: {e}. This error is no good.")
+        return []
+
+def generate_ssl_blacklist_html():
+    print("hold")
 
 def main():
-    print("test")
+    print(fetch_ssl_blacklist())
 
 if __name__ == "__main__":
     main()
